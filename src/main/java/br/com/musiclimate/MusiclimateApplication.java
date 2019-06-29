0a24ef9049e84571514f8c61167b30380635640b
Henrique Teixeira Arroyo <vnthete@Henrique-Macbook-pro.local>
2019-06-29 16:09:13 -0300

    Add initial project structure

---
+++ src/main/java/br/com/musiclimate/MusiclimateApplication.java
@@ -0,0 +1,13 @@
+package br.com.musiclimate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusiclimateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusiclimateApplication.class, args);
+	}
+
+}
