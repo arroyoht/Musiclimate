37192f42c35a03514424ea8993f0f6a87078e1de
Henrique Teixeira Arroyo <vnthete@Henrique-Macbook-pro.local>
2019-06-30 16:31:05 -0300

    Add some logging for debugging

--- src/main/java/br/com/musiclimate/MusiclimateApplication.java
+++ src/main/java/br/com/musiclimate/MusiclimateApplication.java
@@ -2,8 +2,13 @@ package br.com.musiclimate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
+import feign.Logger;
+
+@EnableCaching
 @EnableFeignClients
 @SpringBootApplication(exclude = {
 		org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
@@ -11,5 +16,10 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 public class MusiclimateApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MusiclimateApplication.class, args);
+	}
+
+	@Bean
+	Logger.Level feignLoggerLevel() {
+		return Logger.Level.BASIC;
 	}
 }
