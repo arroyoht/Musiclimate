7f18bdf8a8944817f90fbdf12422ae9259b90a2b
Henrique Teixeira Arroyo <vnthete@Henrique-Macbook-pro.local>
2019-06-30 23:49:26 -0300

    Separate services for better caching strategy and improve redis configuration

--- src/main/java/br/com/musiclimate/service/MusiclimateService.java
+++ src/main/java/br/com/musiclimate/service/MusiclimateService.java
@@ -1,22 +1,19 @@
 package br.com.musiclimate.service;
 
-import java.security.InvalidParameterException;
-import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
-
 import br.com.musiclimate.domain.MusicCategory;
-import br.com.musiclimate.error.ResourceNotFoundException;
 import br.com.musiclimate.error.ServiceUnavailableException;
 import feign.FeignException;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Service
 public class MusiclimateService {
-
+	
 	@Autowired
 	private MusicService musicService;
 
@@ -31,8 +28,6 @@ public class MusiclimateService {
 	 * @param lon longitude
 	 * @return List of playlist song names
 	 */
-	@HystrixCommand(commandKey = "playlistByCoordinates", fallbackMethod = "getFallbackPlaylist", ignoreExceptions = {
-			ResourceNotFoundException.class, InvalidParameterException.class })
 	public List<String> getPlaylistByCoordinates(Double lat, Double lon) {
 		try {
 			Double temp = temperatureService.getTemperatureByCoordinate(lat, lon);
@@ -48,8 +43,6 @@ public class MusiclimateService {
 	 * @param cityName
 	 * @return List of playlist song names
 	 */
-	@HystrixCommand(commandKey = "playlistByCity", fallbackMethod = "getFallbackPlaylist", ignoreExceptions = {
-			ResourceNotFoundException.class, InvalidParameterException.class })
 	public List<String> getPlaylistByCity(String city) {
 		try {
 			Double temp = temperatureService.getTemperatureByCity(city);
@@ -57,36 +50,6 @@ public class MusiclimateService {
 		} catch (FeignException e) {
 			throw new ServiceUnavailableException("Service Unavailable");
 		}
-	}
-
-	// TODO: Improve fallback strategy
-	
-	public List<String> getFallbackPlaylist(String city) {
-		return Arrays.asList("Beautiful People (feat. Khalid)", "Señorita", "Panini", "Call You Mine (with Bebe Rexha)",
-				"On A Roll", "You Need To Calm Down", "Never Really Over", "Only Human",
-				"One Thing Right - feat. Kane Brown", "I Don't Care (with Justin Bieber)", "bad guy", "Rodeo",
-				"Money In The Grave (Drake ft. Rick Ross)", "Post Malone (feat. RANI)",
-				"Summer Days (feat. Macklemore & Patrick Stump of Fall Out Boy)", "Find U Again (feat. Camila Cabello)",
-				"QUE PRETENDES", "Higher Love", "Heaven", "Someone You Loved", "It's You", "Mad Love",
-				"Don't Check On Me (feat. Justin Bieber & Ink)", "Focus (feat. 21 Savage)",
-				"I Think I'm OKAY (with YUNGBLUD & Travis Barker)", "Go Loko", "Old Town Road - Remix", "Rescue Me",
-				"All Around The World (La La La)", "The Git Up", "Mother's Daughter", "If I Can't Have You",
-				"Cross Me (feat. Chance the Rapper & PnB Rock)", "Loco Contigo (with J. Balvin & Tyga)", "Easier",
-				"No Guidance (feat. Drake)", "Sanctuary", "Civil War", "Ritual", "Sad Song (feat. TINI)");
-	}
-
-	public List<String> getFallbackPlaylist(Double lat, Double lon) {
-		return Arrays.asList("Beautiful People (feat. Khalid)", "Señorita", "Panini", "Call You Mine (with Bebe Rexha)",
-				"On A Roll", "You Need To Calm Down", "Never Really Over", "Only Human",
-				"One Thing Right - feat. Kane Brown", "I Don't Care (with Justin Bieber)", "bad guy", "Rodeo",
-				"Money In The Grave (Drake ft. Rick Ross)", "Post Malone (feat. RANI)",
-				"Summer Days (feat. Macklemore & Patrick Stump of Fall Out Boy)", "Find U Again (feat. Camila Cabello)",
-				"QUE PRETENDES", "Higher Love", "Heaven", "Someone You Loved", "It's You", "Mad Love",
-				"Don't Check On Me (feat. Justin Bieber & Ink)", "Focus (feat. 21 Savage)",
-				"I Think I'm OKAY (with YUNGBLUD & Travis Barker)", "Go Loko", "Old Town Road - Remix", "Rescue Me",
-				"All Around The World (La La La)", "The Git Up", "Mother's Daughter", "If I Can't Have You",
-				"Cross Me (feat. Chance the Rapper & PnB Rock)", "Loco Contigo (with J. Balvin & Tyga)", "Easier",
-				"No Guidance (feat. Drake)", "Sanctuary", "Civil War", "Ritual", "Sad Song (feat. TINI)");
 	}
 
 	private MusicCategory getMusicCategory(Double temperature) {
--- src/main/java/br/com/musiclimate/service/TemperatureService.java
+++ src/main/java/br/com/musiclimate/service/TemperatureService.java
@@ -1,12 +1,16 @@
 package br.com.musiclimate.service;
 
 import java.security.InvalidParameterException;
+import java.util.Random;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 import br.com.musiclimate.client.resource.WeatherResource;
 import br.com.musiclimate.client.service.WeatherApiIntegrationService;
@@ -15,9 +19,12 @@ import br.com.musiclimate.error.ResourceNotFoundException;
 @Service
 public class TemperatureService {
 
-	@Autowired 
-	private WeatherApiIntegrationService weatherApiService;
+	private static Double MIN_TEMP = -40.0d;
+	private static Double RANGE = 100.0d;
 	
+	@Autowired
+	private WeatherApiIntegrationService weatherApiService;
+
 	/**
 	 * Method responsible for getting the temperature given a city name
 	 * 
@@ -25,6 +32,8 @@ public class TemperatureService {
 	 * @return temperature value
 	 */
 	@Cacheable(value = "temperatureByCity", key = "#city")
+	@HystrixCommand(commandKey = "getTemperatureByCity", fallbackMethod = "getFallbackTemperature", ignoreExceptions = {
+			ResourceNotFoundException.class, InvalidParameterException.class })
 	public Double getTemperatureByCity(String city) {
 		if (StringUtils.isEmpty(city)) {
 			throw new InvalidParameterException("City name is required");
@@ -45,7 +54,9 @@ public class TemperatureService {
 	 * @param lon longitude
 	 * @return temperature value
 	 */
-	@Cacheable(value = "temperatureByCoordinate", key = "{#lat,#lon}", unless = "#result == null")
+	@Cacheable(value = "temperatureByCoordinate", key = "{#lat,#lon}")
+	@HystrixCommand(commandKey = "getTemperatureByCoordinate", fallbackMethod = "getFallbackTemperature", ignoreExceptions = {
+			ResourceNotFoundException.class, InvalidParameterException.class })
 	public Double getTemperatureByCoordinate(Double lat, Double lon) {
 		if (lat == null || lon == null) {
 			throw new InvalidParameterException("location coordinates is required");
@@ -57,5 +68,21 @@ public class TemperatureService {
 		}
 
 		return resource.getWeather().getTemperature();
+	}
+	
+	public Double getFallbackTemperature(String city) {
+		return getFallbackTemperature();
+	}
+	
+	public Double getFallbackTemperature(Double lat, Double lon) {
+		return getFallbackTemperature();
+	}
+	
+	/**
+	 * Generates random Double ranging from (MIN_TEMP) to (RANGE + MIN_TEMP) 
+	 * @return random temperature
+	 */
+	private Double getFallbackTemperature() {
+		return (new Random().nextDouble() * RANGE) + MIN_TEMP;
 	}
 }
